dca7e14e9322b030f5ce87eaa43ac1ce11076632
objects76 <objects76@gmail.com>
2023-03-09 15:17:27 +0900

    add link bash script

---
+++ link.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+srcpath=$(realpath "${BASH_SOURCE:-$0}")
+srcdir=$(dirname $srcpath)
+
+echo script: $srcpath
+echo srcdir: $srcdir
+
+echo curdir is $(pwd)
+ln -sf $srcdir/scope_fn.py .
+ln -sf $srcdir/dbg.py .
+
+ls -l scope_fn.py dbg.py
+
