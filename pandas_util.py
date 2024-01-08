a544babbe5fca1cbd9b3c8dcec5493ec70538183
objects76 <objects76@gmail.com>
2024-01-08 09:21:15 +0900

    ..

--- pandas_util.py
+++ pandas_util.py
@@ -7,5 +7,5 @@ import pandas as pd
 def apply_comma(df:pd.DataFrame, inplace=False):
     numeric_columns = df.select_dtypes(include=['number']).columns
     tmp = df if inplace else df.copy()
-    tmp[numeric_columns] = df[numeric_columns].applymap('{:,}'.format)
+    tmp[numeric_columns] = df[numeric_columns].map('{:,}'.format)
     return tmp
