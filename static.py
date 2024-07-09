374b812739a220e39e81be6cb127d84527e79c9b
objects76 <objects76@gmail.com>
2024-07-09 13:54:31 +0900

    ..

--- static.py
+++ static.py
@@ -1,6 +1,7 @@
 
 import inspect
 
+# dynamic alloc
 def get_local_static(name, default, obj = None):
     if obj is None:
         caller_frame = inspect.currentframe().f_back
@@ -11,3 +12,11 @@ def get_local_static(name, default, obj = None):
         setattr(obj, name, default)
     return getattr(obj, name)
 
+
+# decorator
+def static_vars(**kwargs):
+    def decorate(func):
+        for k, v in kwargs.items():
+            setattr(func, k, v)
+        return func
+    return decorate
