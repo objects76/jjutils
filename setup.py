b41fe9fda2a66210a6434d391abc0e64489dc89f
김정진 <jjkim@rsupport.com>
2025-05-15 12:02:13 +0900

    ..

--- setup.py
+++ setup.py
@@ -34,11 +34,4 @@ setup(
         # 필요한 의존성 패키지들을 여기에 추가하세요
         # 예: "numpy>=1.20.0",
     ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
 )
